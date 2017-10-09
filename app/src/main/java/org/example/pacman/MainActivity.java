290b173b684c796784b694a8007585f606e5a667
zaifrun <martinknud@gmail.com>
2017-10-09 12:37:32 +0200

    added menu

--- app/src/main/java/org/example/pacman/MainActivity.java
+++ app/src/main/java/org/example/pacman/MainActivity.java
@@ -3,8 +3,11 @@ package org.example.pacman;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import static android.R.attr.button;
 
@@ -39,5 +42,27 @@ public class MainActivity extends Activity {
         });
 
 
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Toast.makeText(this,"settings clicked",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
